5f51043d99f039f0165015e6ba085ca9e15c476c
yangyihan <yangyihan@primeledger.cn>
2018-11-09 18:09:27 +0800

    .

---
+++ index.js
@@ -0,0 +1,41 @@
+const Koa = require('koa')
+const app = new Koa()
+const router = require('koa-router')()
+const bodyParser = require('koa-bodyparser')
+const fs = require('fs')
+var files = fs.readdirSync(__dirname + '/controllers');
+
+var js_files = files.filter((f) => {
+    return f.endsWith('.js');
+});
+
+for (let file of js_files) {
+    let mapping = require(__dirname + '/controllers/' + file)
+    for (let url in mapping) {
+        if (url.startsWith('GET')) {
+            let path = url.substring(4)
+            router.get(path,mapping[url])
+        } else if(url.startsWith('POST')) {
+            let path = url.substring(5)
+            router.post(path,mapping[url])
+        }else {
+            console.log('invalid url: ${url}')
+        }
+    }
+}
+
+app.use(bodyParser())
+app.use(router.routes());
+
+
+// x-respogjgknse-time
+
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+
+app.listen(3000);

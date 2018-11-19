0c38c7cb6b8853129a4be0b91a4249fa52481077
yangyihan <yangyihan@primeledger.cn>
2018-11-19 18:23:14 +0800

    .

--- controllers/activity.js
+++ controllers/activity.js
@@ -1,5 +1,6 @@
 const Monk = require('monk')
 const db = new Monk('localhost:27017/runoob').get('activityList');//链接到活动数据库
+var ObjectId = require('mongodb').ObjectID;
 
 
 /**
@@ -8,7 +9,12 @@ const db = new Monk('localhost:27017/runoob').get('activityList');//链接到活
  */
 
 var getActivity = async (ctx, next) => {
-    await db.find({}).then(res => {
+    // 获取翻页信息
+    let page = {
+        limit: Number(ctx.query.pageSize),
+        skip: (ctx.query.pageNum - 1) * ctx.query.pageSize
+    }
+    await db.find({}, page).then(res => {
         ctx.response.body = {
             code: 2000,
             result: res
@@ -20,21 +26,24 @@ var getActivity = async (ctx, next) => {
 /**
  * 获取详情
  * @param ctx
- * @param next
- * @returns {Promise<void>}
  */
 
 var queryDetail = async (ctx, next) => {
-    var parmas = {
-        _id: ObjectId(ctx.params.id)
+    let parmas = {
+        _id: ObjectId(ctx.query.id)
     }
     await db.find(parmas).then(res => {
         ctx.response.body = {
             code: 2000,
-            result: res
+            result: res,
+            msg:'查询活动成功'
+        }
+    }).catch(err=>{
+        ctx.response.body = {
+            code: 4000,
+            msg:'查询失败,err:' + err
         }
     })
-    console.log('查询成功')
 }
 
 /**
@@ -46,10 +55,15 @@ var addActivity = async (ctx, next) => {
     db.insert(ctx.request.body).then(res => {
         ctx.response.body = {
             code: 2000,
-            result: res
+            result: res,
+            msg:'添加活动成功'
+        }
+    }).catch(err=>{
+        ctx.response.body = {
+            code: 4000,
+            msg:'添加失败,err:' + err
         }
     })
-    console.log('添加活动成功')
 }
 
 
